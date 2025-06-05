66cf2b09bae9db75d5f509b54881a17b9dd9fb6d
amoljagtap17 <amoljagtap17@gmail.com>
2025-06-05 23:15:09 +0530

    nest-graphql-federation: remove boilerplate code for posts app

--- apps/posts-app/src/posts-app.module.ts
+++ apps/posts-app/src/posts-app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PostsAppController } from './posts-app.controller';
-import { PostsAppService } from './posts-app.service';
 
 @Module({
   imports: [],
-  controllers: [PostsAppController],
-  providers: [PostsAppService],
+  controllers: [],
+  providers: [],
 })
 export class PostsAppModule {}
--- apps/posts-app/src/posts-app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PostsAppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
