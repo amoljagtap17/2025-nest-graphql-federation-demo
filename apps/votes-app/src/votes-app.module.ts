53aca1864ac3f4bc57755111cd39aaac9759fdba
amoljagtap17 <amoljagtap17@gmail.com>
2025-06-10 22:50:40 +0530

    nest-graphql-federation: add votes module to votes app

--- apps/votes-app/src/votes-app.module.ts
+++ apps/votes-app/src/votes-app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [],
+  imports: [VotesModule],
   controllers: [],
   providers: [],
 })
